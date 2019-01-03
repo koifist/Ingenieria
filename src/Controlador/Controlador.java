2e9856246bd61e894e67af1f2336afdc47d7a69a
Fernando <Fernando@DESKTOP-QTRFBFB>
2019-01-03 20:30:48 +0100

    prueba

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -11,7 +11,6 @@ public class Controlador{
 	public static void main(String[] args) throws SQLException {
 		Conector cn = new Conector();
 		Usuario usr = cn.getUser("pepito");
-	System.out.println(usr.getId()+" "+usr.getApellido1());
 		ArrayList<Compra> compras = cn.getCompras(usr.getId());
 		System.out.println(compras.get(0).getFecha());
 		}
